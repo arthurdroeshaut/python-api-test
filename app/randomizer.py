f3f348ab36a0ae3bf4cdfa2d953632290b981740
arthurdroeshaut <r0750378@student.thomasmore.be>
2022-11-16 13:21:50 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -1,28 +1,25 @@
 from fastapi import FastAPI
-from random import randint
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+class Login(BaseModel):
+    username: str
+    password: str
 
 app = FastAPI()
+origins = [""]
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=[""], allow_headers=["*"])
 
-@app.get("/percentage")
-async def get_random_percentage():
-    return {'percentage': randint(0, 100)}
+@app.get("/")
+def root():
+    return {"message": "endpoint 1"}
 
-@app.get("/percentage/{lower_limit}/{upper_limit}")
-async def get_random_percentage1(lower_limit: int, upper_limit: int):
-    if(lower_limit >= upper_limit):
-        return {'error': 'The upper limit must be greater than the lower limit!', 'lower limit': lower_limit, 'upper limit': upper_limit}
-    return {'percentage': randint(lower_limit, upper_limit)}
+@app.get("/")
+def endpoint1():
+    return {"message": "endpoint 2"}
 
-@app.get("/percentage/{lower_limit}/{upper_limit}/{amount}")
-async def get_random_percentage2(lower_limit: int, upper_limit: int, amount: int):
-    if(lower_limit >= upper_limit):
-        return {'error': 'The upper limit must be greater than the lower limit!', 'lower limit': lower_limit, 'upper limit': upper_limit}
-
-    if (amount <= 0):
-        return {'error': 'The amount must be greater than 0!', 'amount': amount}
-
-    random_numbers = []
-    for counter in range(amount):
-        random_numbers.append(randint(lower_limit, upper_limit))
-
-    return {'percentages': random_numbers}
+@app.post("/test")
+def postLogin(credentials: Login):
+    return credentials
