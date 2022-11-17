d54a1ef2c1febf42215bc84d670ea6dceb1305d7
arthurdroeshaut <r0750378@student.thomasmore.be>
2022-11-17 19:10:05 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -2,32 +2,31 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 app = FastAPI()
-class BusinessCard(BaseModel):
-    first_name: str
-    last_name: str
-    street: str
-    number: str = Field(max_length=10)
-    addition: str | None = None
-    zip: int
-    city: str
-business_card_brent = {
-    "first_name": "Brent",
-    "last_name": "Pulmans",
-    "street": "Kleinhoefstraat",
-    "number": "4",
-    "zip": 2440,
-    "city": "Geel"
+class voetbalspeler(BaseModel):
+    voornaam: str
+    achternaam: str
+    nationaliteit: str
+    positie: str
+    leeftijd: int
+
+voetballer_Ronaldo = {
+    "id": 1,
+    "voornaam": "Cristiano",
+    "achternaam": "Ronaldo",
+    "nationaliteit": "Portugees",
+    "positie": "aanvaller",
+    "leeftijd": 37,
 }
-cards = {0: business_card_brent}
-@app.post("/cards")
-async def create_item(card: BusinessCard):
-    new_key = max(cards, key=cards.get)+1
-    cards[new_key] = card
-    return cards[new_key]
-@app.get("/cards")
+speler = {0: voetballer_Ronaldo}
+@app.post("/voetballer")
+async def create_item(voetballer: voetbalspeler):
+    new_key = max(speler, key=voetballer.get)
+    voetballer[new_key] = speler
+    return voetballer[new_key]
+@app.get("/voetballer")
 async def get_item():
-    return cards
-@app.put("/cards/{id}")
-async def get_item(id: int, card: BusinessCard):
-    cards[id] = card
-    return cards[id]
+    return speler
+@app.put("/voetballer/{id}")
+async def get_item(id: int, voetballer: voetbalspeler):
+    voetballer[id] = speler
+    return voetballer[id]
