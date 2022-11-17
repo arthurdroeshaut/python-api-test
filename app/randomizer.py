42331248bbc67c13ef29dc8e0e52ce8d82652821
arthurdroeshaut <r0750378@student.thomasmore.be>
2022-11-17 17:28:22 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -1,16 +1,19 @@
-
 from fastapi import FastAPI
 from random import randint
+
 app = FastAPI()
 @app.get("/percentage")
+
 async def get_random_percentage():
     return {'percentage': randint(0, 100)}
+
 @app.get("/percentage/{lower_limit}/{upper_limit}")
 async def get_random_percentage(lower_limit: int, upper_limit: int):
     if(lower_limit >= upper_limit):
         return {'error': 'The upper limit must be greater than the lower limit!', 
 'lower limit': lower_limit, 'upper limit': upper_limit}
     return {'percentage': randint(lower_limit, upper_limit)}
+
 @app.get("/percentage/{lower_limit}/{upper_limit}/{amount}")
 async def get_random_percentage(lower_limit: int, upper_limit: int, amount: int):
     if(lower_limit >= upper_limit):
