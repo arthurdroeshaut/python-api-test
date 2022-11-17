c277ed764bd27bb4bc6a8626e6184a40ecfec2fa
arthurdroeshaut <r0750378@student.thomasmore.be>
2022-11-17 21:07:07 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -5,49 +5,46 @@ app = FastAPI()
 class voetbalspeler(BaseModel):
     voornaam: str
     achternaam: str
+    ploeg: str
     nationaliteit: str
     positie: str
     leeftijd: int
 
-alle_voetballers = [ "Cristiano", "Messi", "Mbappe", "Hazard", "De Bruyne", "Neymar", "Reus"]
-    
 
+alle_voetballers = [ "Cristiano" "Messi" "Mbappe" "Hazard" "De Bruyne" "Neymar" "Reus"]
+
+#geeft meer info weer over ronaldo om deze later op te vragen
 voetballer_Ronaldo = {
     "voornaam": "Cristiano",
     "achternaam": "Ronaldo",
+    "ploeg": "Manchester United",
     "nationaliteit": "Portugees",
     "positie": "aanvaller",
     "leeftijd": 37,
 }
-
+#geeft meer info weer over messi om deze later op te vragen
 voetballer_Messi = {
     "voornaam": "Lionel",
     "achternaam": "Messi",
+    "ploeg": "Paris Saint German",
     "nationaliteit": "Argentijns",
     "positie": "aanvaller",
     "leeftijd": 35,
 }
-speler = {1: voetballer_Ronaldo}
-speler = {2: voetballer_Messi}
-@app.post("/voetballer/Ronaldo")
-async def create_item(voetballer: voetbalspeler):
-    new_key = max(speler, key=voetballer.get)
-    voetballer[new_key] = speler
-    return voetballer[new_key]
 
+
+#ik wil eerst een lijst opvragen van alle mogelijke spelers
 @app.get("/voetballer")
 async def get_item():
     return alle_voetballers
 
+#nu wil ik een lijst opvragen met extra info over de speler "Ronaldo"
 @app.get("/voetballer/Ronaldo")
 async def get_item():
     return voetballer_Ronaldo
 
+#nu wil ik een lijst opvragen met extra info over de speler "Messi"
 @app.get("/voetballer/Messi")
 async def get_item():
     return voetballer_Messi
 
-@app.put("/voetballer/{id}")
-async def get_item(id: int, voetballer: voetbalspeler):
-    voetballer[id] = speler
-    return voetballer[id]
