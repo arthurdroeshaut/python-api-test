ce5e3da2968f9ab4516fc65bb8b2b2292f69030b
arthurdroeshaut <r0750378@student.thomasmore.be>
2022-11-18 21:35:14 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -6,21 +6,26 @@ import random
 
 app = FastAPI()
 class StarWars(BaseModel):
+    Star_Wars_Characters = str
+    Lightsaber_Colors = str
+    Birthplace = str
+    Species = str
+    Rank = str
     
 
-    star_wars_characters = [ "Anakin Skywalker", "Darth Vader", "Chewbacca", "Han Solo", "Obi-Wan-Kenobi", "Leia Organa", "Ahsoka Tano", "R2D2"]
-    Lightsaber_Colors = [ "Green," "Blue", "red","Purple", "White", "Yellow", "Black"]
-    Birthplace = ["Coruscant", "Naboo", "Batuu", "Tattooine","Kashyyyk", "Cato Neimoidia"]
-    Species = ["Human", "Jawa", "Ewok", "Wookie", "Gungan", "Rodian", "Kaminoan"]
-    Rank = ["Youngling", "Padawan", "Jedi Knight", "Jedi Master", "Sith Acolyte","Sith Master", "Sith Lord", "Dark Council", "Jedi Grand Master",]
+    Star_Wars_Characters = [ "Anakin Skywalker", "Darth Vader", "Chewbacca", "Han Solo", "Obi-Wan-Kenobi", "Leia Organa", "Ahsoka Tano", "R2D2"],
+    Lightsaber_Colors = [ "Green," "Blue", "red","Purple", "White", "Yellow", "Black"],
+    Birthplace = ["Coruscant", "Naboo", "Batuu", "Tattooine","Kashyyyk", "Cato Neimoidia"],
+    Species = ["Human", "Jawa", "Ewok", "Wookie", "Gungan", "Rodian", "Kaminoan"],
+    Rank = ["Youngling", "Padawan", "Jedi Knight", "Jedi Master", "Sith Acolyte","Sith Master", "Sith Lord", "Dark Council", "Jedi Grand Master",],
 
 
 
 
-#ik wil eerst een random voetballer opvragen
+#ik wil eerst een random star wars karakter opvragen
 @app.get("/StarWars")
-async def get_item():
-    return random.choice(star_wars_characters)
+async def get_item(Star_Wars_Characters):
+    return random.choice(Star_Wars_Characters)
 
 
 
