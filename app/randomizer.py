5a9200fbfd9c169eec0fc107153c1c2615895b21
arthurdroeshaut <r0750378@student.thomasmore.be>
2022-11-19 23:25:41 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -34,9 +34,8 @@ class StarWars(BaseModel):
 
 
 # lijsten voor op te vragen in de api
-Star_Wars_Characters = ["123", "321"]
-    #["Anakin Skywalker", "Darth Vader", "Chewbacca", "Han Solo", "Obi-Wan-Kenobi", "Leia Organa",
-                       # "Ahsoka Tano", "R2D2"]
+Star_Wars_Characters = ["Anakin Skywalker", "Darth Vader", "Chewbacca", "Han Solo", "Obi-Wan-Kenobi", "Leia Organa",
+                        "Ahsoka Tano", "R2D2"]
 Lightsaber_Colors = ["Green", "Blue", "red", "Purple", "White", "Yellow", "Black"]
 Birthplaces = ["Coruscant", "Naboo", "Batuu", "Tattooine", "Kashyyyk", "Cato Neimoidia", "unknown"]
 Species = ["Human", "Jawa", "Ewok", "Wookie", "Gungan", "Rodian", "Kaminoan"]
@@ -50,10 +49,12 @@ Ships = ["Millenium Falcon", "Death Star", "Star Destroyer", "Slave 1", "TIE fig
 async def get_item():
     return random.choice(Star_Wars_Characters)
 
+
 # ik wil hierna een random LightSaber uit de lijst opvragen.
 @app.get("/StarWars/Characters/LightsaberColor")
 async def get_items():
     return random.choice(Lightsaber_Colors)
+
 
 # hierna een random geboorteplaats (dit is grotendeels om te zien of dit werkt
 @app.get("/StarWars/Characters/Birthplace")
