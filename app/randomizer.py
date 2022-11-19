4b58455ed527912ae55eb6c237bc4af27baee87c
arthurdroeshaut <r0750378@student.thomasmore.be>
2022-11-19 22:49:25 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -34,8 +34,9 @@ class StarWars(BaseModel):
 
 
 # lijsten voor op te vragen in de api
-Star_Wars_Characters = ["Anakin Skywalker", "Darth Vader", "Chewbacca", "Han Solo", "Obi-Wan-Kenobi", "Leia Organa",
-                        "Ahsoka Tano", "R2D2"]
+Star_Wars_Characters = ["123", "321"]
+    #["Anakin Skywalker", "Darth Vader", "Chewbacca", "Han Solo", "Obi-Wan-Kenobi", "Leia Organa",
+                       # "Ahsoka Tano", "R2D2"]
 Lightsaber_Colors = ["Green", "Blue", "red", "Purple", "White", "Yellow", "Black"]
 Birthplaces = ["Coruscant", "Naboo", "Batuu", "Tattooine", "Kashyyyk", "Cato Neimoidia", "unknown"]
 Species = ["Human", "Jawa", "Ewok", "Wookie", "Gungan", "Rodian", "Kaminoan"]
